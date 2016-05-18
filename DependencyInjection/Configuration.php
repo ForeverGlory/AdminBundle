bba879c96fbfb44e2ea40a6f9f7d1db13cc0fa9c
ForeverGlory <foreverglory@qq.com>
2016-05-19 01:25:11 +0800

    增加配置

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,17 +1,30 @@
 <?php
 
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Glory\Bundle\AdminBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ * 
+ * @author ForeverGlory <foreverglory@qq.com>
  */
 class Configuration implements ConfigurationInterface
 {
+
     /**
      * {@inheritdoc}
      */
@@ -19,11 +32,73 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('glory_admin');
+        
+        $rootNode->children()
+                    ->scalarNode('role')->isRequired()->defaultValue('ROLE_ADMIN')->end()
+                ->end();
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
+        $this->addStylesheetsConfiguration($rootNode);
+        $this->addJavascriptsConfiguration($rootNode);
+        $this->addNavigationConfiguration($rootNode);
+        $this->addSidebarConfiguration($rootNode);
+        $this->addDashboardConfiguration($rootNode);
+        
         return $treeBuilder;
     }
+
+    protected function addStylesheetsConfiguration(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->arrayNode('stylesheets')
+                ->prototype('scalar')
+                ->end()
+            ->end();
+    }
+    
+    protected function addJavascriptsConfiguration(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->arrayNode('javascripts')
+                ->prototype('scalar')
+                ->end()
+            ->end();
+    }
+    
+    protected function addNavigationConfiguration(ArrayNodeDefinition $node){
+        $node
+            ->children()
+                ->arrayNode('navigation')
+                ->end()
+            ->end();
+    }
+    
+    protected function addSidebarConfiguration(ArrayNodeDefinition $node){
+        $node
+            ->children()
+                ->arrayNode('sidebar')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                    ->children()
+                        ->scalarNode('title')->end()
+                        ->scalarNode('icon')->defaultValue('fa fa-list')->end()
+                        ->scalarNode('route')->end()
+                        ->arrayNode('parameters')->end()
+                        ->scalarNode('path')->end()
+                        ->scalarNode('target')->defaultValue('')->end()
+                        ->scalarNode('children')->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+    
+    protected function addDashboardConfiguration(ArrayNodeDefinition $node){
+        $node
+            ->children()
+                ->arrayNode('dashboard')
+                ->end()
+            ->end();
+    }
+
 }
--- DependencyInjection/GloryAdminExtension.php
+++ DependencyInjection/GloryAdminExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Glory\Bundle\AdminBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -11,9 +20,14 @@ use Symfony\Component\DependencyInjection\Loader;
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * 
+ * @author ForeverGlory <foreverglory@qq.com>
  */
 class GloryAdminExtension extends Extension
 {
+
+    protected $menu = [];
+
     /**
      * {@inheritdoc}
      */
@@ -22,7 +36,46 @@ class GloryAdminExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+
+        $container->setParameter('glory_admin.role', $config['role']);
+
+        foreach ($config['stylesheets'] as $stylesheet) {
+            $this->addStylesheet($container, $stylesheet);
+        }
+        foreach ($config['javascripts'] as $javascript) {
+            $this->addJavascript($container, $javascript);
+        }
+
+        foreach ($config['sidebar'] as $name => $sidebar) {
+            $this->addSidebar($container, $name, $sidebar);
+        }
+        foreach ($config['navigation'] as $name => $navigation) {
+            $this->addNavigation($container, $name, $navigation);
+        }
     }
+
+    public function addStylesheet(ContainerBuilder $container, $stylesheet)
+    {
+        $container->getDefinition('glory_admin.admin')
+                ->addMethodCall('addStylesheet', array($stylesheet));
+    }
+
+    public function addJavascript(ContainerBuilder $container, $javascript)
+    {
+        $container->getDefinition('glory_admin.admin')
+                ->addMethodCall('addJavascript', array($javascript));
+    }
+
+    public function addSidebar(ContainerBuilder $container, $name, $sidebar = [])
+    {
+        
+    }
+
+    public function addNavigation(ContainerBuilder $container, $name, $navigation = [])
+    {
+        
+    }
+
 }
