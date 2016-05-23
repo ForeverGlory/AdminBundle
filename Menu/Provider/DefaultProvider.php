287fa1f7c93ca81da7b6fcf065893fbe2ad44c42
ForeverGlory <foreverglory@qq.com>
2016-05-23 20:00:13 +0800

    直接使用函数

--- Menu/Provider/DefaultProvider.php
+++ Menu/Provider/DefaultProvider.php
@@ -41,18 +41,18 @@ class DefaultProvider implements MenuProviderInterface
         $navigation = $this->menuManager->createMenu();
         $navigation->setName('admin_navigation');
         $navigation->setLabel('Admin Navigation');
-        $navigation->setAttribute('template', 'GloryAdminBundle:Menu:navigation.html.twig');
+        $navigation->setTemplate('GloryAdminBundle:Menu:navigation.html.twig');
 
         $sidebar = $this->menuManager->createMenu();
         $sidebar->setName('admin_sidebar');
         $sidebar->setLabel('Admin Sidebar');
-        $sidebar->setAttribute('template', 'GloryAdminBundle:Menu:sidebar.html.twig');
+        $sidebar->setTemplate('template', 'GloryAdminBundle:Menu:sidebar.html.twig');
         $dashboard = $this->menuManager->createMenu();
         $dashboard->setName('dashboard');
         $dashboard->setLabel('Dashboard');
-        $dashboard->setRoute('glory_admin');
-        $dashboard->setUri($router->generate('glory_admin'));
-        $dashboard->setAttribute('icon', 'fa fa-dashboard');
+        $dashboard->setRoute('glory_admin_dashboard');
+        $dashboard->setUri($router->generate('glory_admin_dashboard'));
+        $dashboard->setIcon('fa fa-dashboard');
         $sidebar->addChild($dashboard);
 
         $this->menus = array(
