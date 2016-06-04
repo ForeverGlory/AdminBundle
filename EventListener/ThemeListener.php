8a2931d1491f713044be2994a04f50809d29e0a2
ForeverGlory <foreverglory@qq.com>
2016-06-05 06:08:47 +0800

    记录一下 本来准备使用事件，来处理模板，却无法在事件中取得模板参数，无法继续

---
+++ EventListener/ThemeListener.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\AdminBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Glory\Bundle\AdminBundle\Variable\Admin;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Bridge\Twig\DataCollector\TwigDataCollector;
+
+/**
+ * Description of ThemeListener
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class ThemeListener implements EventSubscriberInterface
+{
+
+    protected $twig;
+    protected $collector;
+    protected $admin;
+
+    public function __construct(\Twig_Environment $twig, TwigDataCollector $collector, Admin $admin)
+    {
+        $this->twig = $twig;
+        $this->collector = $collector;
+        $this->admin = $admin;
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+
+        $response = $event->getResponse();
+        $request = $event->getRequest();
+
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+        if ($request->isXmlHttpRequest()) {
+            return;
+        }
+
+        if ($this->admin->inAdmin()) {
+            $templates = $this->collector->getTemplates();
+            $response->setContent($this->twig->render('GloryAdminBundle::layout.html.twig'));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            //KernelEvents::RESPONSE => array('onKernelResponse', 128),
+        );
+    }
+
+}
