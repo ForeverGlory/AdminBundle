c877772f4704ff95e25ebec1e8cef02787728fe3
ForeverGlory <foreverglory@qq.com>
2016-06-05 04:38:11 +0800

    Twig引擎重写，增加render时，判断是否属于后台页面，替换模板

---
+++ DependencyInjection/Compiler/TwigEnginePass.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <https://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\AdminBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * check TwigEngine
+ * 
+ * 重写Twig引擎，扩展后台页面判断
+ * @see \Glory\Bundle\AdminBundle\Twig\Templating\TwigEngine
+ * 
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class TwigEnginePass implements CompilerPassInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $container->setDefinition(
+                'glory_admin.twig_engine', $container->getDefinition('templating.engine.twig')
+        );
+        $container->setDefinition(
+                        'templating.engine.twig', new Definition('Glory\\Bundle\\AdminBundle\\Twig\\Templating\\TwigEngine', [
+                    new Reference('twig'), new Reference('templating.name_parser'), new Reference('templating.locator')
+                ]))
+                ->addMethodCall('setEngine', [new Reference('glory_admin.twig_engine')])
+                ->addMethodCall('setAdmin', [new Reference('glory_admin.admin')])
+        ;
+    }
+
+}
---
+++ Twig/Templating/TwigEngine.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\AdminBundle\Twig\Templating;
+
+use Symfony\Bundle\TwigBundle\TwigEngine as BaseEngine;
+use Symfony\Component\Templating\EngineInterface;
+
+/**
+ * Description of TwigEngine
+ *
+ * Twig引擎重写，增加render时，判断是否属于后台页面，替换模板
+ * 如果属于后台页面，则使用后台模板"GloryAdminBundle::layout.html.twig"，并且加载当前模板里面的 block title/stylesheets/javascripts/content
+ * 
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class TwigEngine extends BaseEngine
+{
+
+    /**
+     * @var EngineInterface 
+     */
+    protected $engine;
+    protected $admin;
+
+    public function setEngine($engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function setAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render($name, array $parameters = array())
+    {
+        if (!$this->admin->inAdmin()) {
+            return $this->engine->render($name, $parameters);
+        }
+        $layout = $this->load('GloryAdminBundle::layout.html.twig');
+        $template = $this->load($name);
+
+        $blocks = [];
+        foreach (['title', 'stylesheets', 'javascripts', 'content'] as $name) {
+            if (false !== $block = $this->getBlock($template, $name)) {
+                $blocks[$name] = $block;
+            }
+        }
+        ob_start();
+        try {
+            $layout->display($parameters, $blocks);
+        } catch (\Exception $e) {
+            ob_end_clean();
+            throw $e;
+        }
+        return ob_get_clean();
+    }
+
+    protected function getBlock(\Twig_Template $template, $name)
+    {
+        if ($template->hasBlock($name)) {
+            $blocks = $template->getBlocks();
+            return $blocks[$name];
+        } elseif (false !== $parent = $template->getParent([])) {
+            return $this->getBlock($parent, $name);
+        }
+        return false;
+    }
+
+    public function exists($name)
+    {
+        return $this->engine->exists($name);
+    }
+
+    public function stream($name, array $parameters = array())
+    {
+        return $this->engine->stream($name, $parameters);
+    }
+
+    public function supports($name)
+    {
+        return $this->engine->supports($name);
+    }
+
+}
