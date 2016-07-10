ef38f80eb35f4b73d4442978254ec47b43f6e942
ForeverGlory <foreverglory@qq.com>
2016-07-10 15:54:27 +0800

    消息接口

---
+++ Controller/MessageController.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of MessageController
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class MessageController extends Controller
+{
+
+    public function indexAction(Request $request)
+    {
+        
+    }
+
+}
